1ac1e56e4dc96adfa680c7c40ae60a4fbc74310f
Julieta Campos <jcampos@itexico.net>
2016-10-15 22:26:04 -0500

    MailiingListComponent

---
+++ src/app/mail-listing/mail-listing.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { MailListingComponent } from './mail-listing.component';
+
+describe('Component: MailListing', () => {
+  it('should create an instance', () => {
+    let component = new MailListingComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/mail-listing/mail-listing.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { GetMailsService } from '../services/get-mails.service';
+import { Mail } from '../shared/mail';
+
+import * as moment from 'moment';
+
+@Component({
+  selector: 'app-mail-listing',
+  templateUrl: './mail-listing.component.html',
+  styleUrls: ['./mail-listing.component.css'],
+  providers: [GetMailsService]
+})
+export class MailListingComponent implements OnInit {
+
+  mails: Mail[] = [];
+  selectedMail: Mail;
+
+  constructor(private getMails: GetMailsService) { }
+
+  ngOnInit() {
+    this.getMails.getMails().then(
+      (mails) => {
+        this.mails = mails.map(mail => {
+          //  format "Mon 06 July, 10:53",
+           mail.time_formatted =  moment.unix(mail.time_sent).format('ddd DD MMMM, HH:mm');
+           return mail;
+        });
+      }
+    );
+  }
+
+  onSelect(mail: Mail){
+    mail.hasBeenRead = true;
+    this.selectedMail = mail;
+  }
+
+  delete(mail: Mail){
+    let index = this.mails.indexOf(mail);
+    this.mails.splice(index, 1);
+  }
+
+}
